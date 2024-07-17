ee6fa2b4f7c28fdc0379609c2779f332be83678f
adi.mikros <adi.mikros@gmail.com>
2024-07-17 10:23:05 +0700

    komit awali

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+   <AppHeader/>
+
+    <NuxtPage/>
+   
+   <AppFooter/>
+
+  </div>
+</template>
+
+<script>
+
+useHead({
+  title:"LATIHAN1",
+  link:[
+    {
+      rel : "stylesheet",
+      href : "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    }, //css bootstrap 5
+    {
+      src : "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+    }
+  ]
+})
+</script>
+
